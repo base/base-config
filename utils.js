04a4761fe1a6a50cddbfcb7287336526712e3cf5
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-04 05:45:19 -0500

    clean up utils

--- utils.js
+++ utils.js
@@ -8,18 +8,6 @@ var path = require('path');
  */
 
 var utils = require('lazy-cache')(require);
-
-/**
- * Temporarily re-assign `require` to trick browserify and
- * webpack into reconizing lazy dependencies.
- *
- * This tiny bit of ugliness has the huge dual advantage of
- * only loading modules that are actually called at some
- * point in the lifecycle of the application, whilst also
- * allowing browserify and webpack to find modules that
- * are depended on but never actually called.
- */
-
 var fn = require;
 require = utils;
 
@@ -30,11 +18,6 @@ require = utils;
 require('isobject', 'isObject');
 require('map-config', 'mapper');
 require('resolve-dir', 'resolve');
-
-/**
- * Restore `require`
- */
-
 require = fn;
 
 /**
@@ -43,13 +26,12 @@ require = fn;
 
 utils.tryRequire = function tryRequire(name, cwd) {
   try {
-    cwd = cwd || process.cwd();
-    return require(path.resolve(cwd, name));
-  } catch (err) {};
-  try {
     return require(name);
   } catch (err) {};
-  return null;
+
+  try {
+    return require(path.resolve(cwd || process.cwd(), name));
+  } catch (err) {};
 };
 
 /**
@@ -57,22 +39,12 @@ utils.tryRequire = function tryRequire(name, cwd) {
  */
 
 utils.arrayify = function(val) {
-  if (typeof val === 'undefined' || val === null || val === '') {
-    return [];
-  }
+  if (!val) return [];
   if (typeof val === 'string') {
     return val.split(',');
   }
   return Array.isArray(val) ? val : [val];
 };
-
-function format(arr) {
-  var res = '';
-  arr.forEach(function(ele) {
-    res += ele + '\'' + '\n';
-  });
-  return res;
-}
 
 /**
  * Expose `utils` modules
